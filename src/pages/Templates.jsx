14b45905491a298351c412beda85368d94490803
LoganC <alclary@posteo.net>
2023-04-24 01:19:41 -0500

    templates list page implemented

--- src/pages/Templates.jsx
+++ src/pages/Templates.jsx
@@ -1,8 +1,21 @@
-// import { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import ListItem from "../components/ListItem";
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from "../functions/db";
+
 export default function Templates() {
-    return (
-        <>
-            <p>Templates</p>
-        </>
-    );
+  const templates = useLiveQuery(async () => {
+    return await db.templates.toArray();
+  });
+
+  return (
+    <>
+      <SearchBar />
+      <div id="templateList" className="ui cards">
+        {templates?.map((template) => (
+          <ListItem key={template.id} template={template} />
+        ))}
+      </div>
+    </>
+  );
 }
