c5fee168f9403631dea0d680ed88d57a5577b9a5
LoganC <alclary@posteo.net>
2023-04-24 20:50:45 -0500

    document JSONSchema examples for template to record microservice

---
+++ src/sampleData/ExampleRecords.js
@@ -0,0 +1,31 @@
+// Example record data 1
+{
+  "title": "Form Checklist",
+  "sections": [
+    {
+      "sectionName": "Section 1",
+      "sectionDesc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "checklist": [
+        {
+          "checkDesc": "Checklist item 1.1 desc",
+          "checkValue": false
+        },
+        {
+          "checkDesc": "Checklist item 1.2 desc",
+          "checkValue": true
+        }
+      ]
+    },
+    {
+      "sectionName": "Section 2",
+      "sectionDesc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "checklist": [
+        {
+          "checkDesc": "Checklist item 2.1 desc",
+          "checkValue": false
+        }
+      ]
+    }
+  ]
+}
+
