03b63af477fe4a0ce0c2aa68e763c36c16c531f6
LoganC <alclary@posteo.net>
2023-04-24 20:50:24 -0500

    add record population to debug page

--- src/pages/SetupDebug.jsx
+++ src/pages/SetupDebug.jsx
@@ -1,15 +1,21 @@
+import RecordClass from "../classes/RecordClass";
 import TemplateClass from "../classes/TemplateClass";
 import { db } from "../functions/db";
 import { useEffect } from "react";
 
 export default function SetupDebug() {
   useEffect(() => {
-    // db.delete();
     const template1 = new TemplateClass("Template 1");
     const template2 = new TemplateClass("Template 2");
 
     db.templates.put(template1);
     db.templates.put(template2);
+
+    const record1 = new RecordClass("ID001238", 17);
+    const record2 = new RecordClass("ID009938", 18);
+
+    db.records.put(record1);
+    db.records.put(record2);
   }, []);
 
   return (
