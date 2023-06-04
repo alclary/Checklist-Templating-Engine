bdf69f53d2871dac687f86f9e02d6f7c080ee364
LoganC <alclary@posteo.net>
2023-06-04 12:16:12 -0500

    debug fresh start script

--- src/pages/SetupDebug.jsx
+++ src/pages/SetupDebug.jsx
@@ -5,17 +5,20 @@ import { useEffect } from "react";
 
 export default function SetupDebug() {
   useEffect(() => {
+    db.templates.clear();
+    db.records.clear();
+
     const template1 = new TemplateClass("Template 1");
     const template2 = new TemplateClass("Template 2");
 
     db.templates.put(template1);
     db.templates.put(template2);
 
-    const record1 = new RecordClass("ID001238", 17);
-    const record2 = new RecordClass("ID009938", 18);
+    const record1 = new RecordClass("ID001238", 1);
+    const record2 = new RecordClass("ID009938", 2);
 
-    db.records.put(record1);
-    db.records.put(record2);
+    record1.init();
+    record2.init();
   }, []);
 
   return (
