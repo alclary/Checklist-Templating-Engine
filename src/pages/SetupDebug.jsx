47535e267850ce302231b70ea9683c8bdfc42f04
LoganC <alclary@posteo.net>
2023-06-05 16:04:23 -0500

    better callbacks for debug page

--- src/pages/SetupDebug.jsx
+++ src/pages/SetupDebug.jsx
@@ -11,14 +11,26 @@ export default function SetupDebug() {
     const template1 = new TemplateClass("Template 1");
     const template2 = new TemplateClass("Template 2");
 
-    db.templates.put(template1);
-    db.templates.put(template2);
+    db.templates
+      .put(template1)
+      .then((id) => {
+        console;
+        const record1 = new RecordClass("ID001238", id);
+        return record1;
+      })
+      .then((record1) => {
+        record1.init();
+      });
 
-    const record1 = new RecordClass("ID001238", 1);
-    const record2 = new RecordClass("ID009938", 2);
-
-    record1.init();
-    record2.init();
+    db.templates
+      .put(template2)
+      .then((id) => {
+        const record2 = new RecordClass("ID009938", id);
+        return record2;
+      })
+      .then((record2) => {
+        record2.init();
+      });
   }, []);
 
   return (
